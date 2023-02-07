145470e1f8b6b63228d2e8c9a972e72686a3df9a
= <ismoilbek20050421@gmail.com>
2023-02-08 00:48:54 +0500

    The End Project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
-for(let btn of $$(".section-card-container-box__img")){
-  btn.addEventListener("dragover",(e)=> {
-    console.log(e.target.value);
-  })
-}
+$(".fa-bars").addEventListener("click", () => {
+  $(".navbars").classList.add("active");
+});
+
+$(".fa-times").addEventListener("click", () => {
+  $(".navbars").classList.remove("active");
+});
